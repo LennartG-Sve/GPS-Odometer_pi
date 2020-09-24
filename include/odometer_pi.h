c8c2a9d578e611df549669a12c55301d54ee6e51
LennartG-Sve <71273808+LennartG-Sve@users.noreply.github.com>
2020-09-24 14:07:19 +0200

    Add files via upload

--- include/odometer_pi.h
+++ include/odometer_pi.h
@@ -38,12 +38,6 @@
   #include "wx/wx.h"
 #endif //precompiled headers
 
-//#define     PLUGIN_VERSION_MAJOR    0
-//#define     PLUGIN_VERSION_MINOR    2
-
-//#define     MY_API_VERSION_MAJOR    1
-//#define     MY_API_VERSION_MINOR    8
-
 #include <wx/notebook.h>
 #include <wx/fileconf.h>
 #include <wx/listctrl.h>
